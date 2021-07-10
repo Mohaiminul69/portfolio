f61d9c9a868dc34e825cc3989328d1386af394c4
mohaiminul69 <mohaiminul69@gmail.com>
2021-07-10 18:53:29 +0600

    cursor upgrade

--- js/mouse.js
+++ js/mouse.js
@@ -3,27 +3,45 @@
 // Mouse
 const body = document.body;
 let mouseCursor = document.querySelector(".cursor");
+const mouseFollower = document.querySelector(".cursor-follower");
 let navLinks = document.querySelectorAll("a");
 
 window.addEventListener("mousemove", cursor);
 
+var cursorDisplay = null;
+function turnOnCursor() {
+  if (cursorDisplay === "block") {
+    return;
+  }
+  cursorDisplay = "block";
+  mouseCursor.style.display = cursorDisplay;
+  mouseFollower.style.display = cursorDisplay;
+}
+
 function cursor(e) {
+  turnOnCursor();
   mouseCursor.style.top = `${e.pageY}px`;
   mouseCursor.style.left = `${e.pageX}px`;
+  mouseFollower.style.top = `${e.pageY}px`;
+  mouseFollower.style.left = `${e.pageX}px`;
 }
 
 body.addEventListener("mouseover", () => {
   mouseCursor.classList.add("cursor");
+  mouseFollower.classList.add("cursor-follower");
 });
 body.addEventListener("mouseleave", () => {
   mouseCursor.classList.remove("cursor");
+  mouseFollower.classList.remove("cursor-follower");
 });
 
 navLinks.forEach((link) => {
   link.addEventListener("mouseover", () => {
     mouseCursor.classList.add("link-grow");
+    mouseFollower.style.display = "none";
   });
   link.addEventListener("mouseleave", () => {
+    mouseFollower.style.display = "block";
     mouseCursor.classList.remove("link-grow");
   });
 });
